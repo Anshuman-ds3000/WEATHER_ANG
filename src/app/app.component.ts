f699e2cec29f0bfa947b2ebf022867a1cb5291d8
Anshuman-ds3000 <codeas01@gmail.com>
2025-02-16 00:54:07 +0530

    Change service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,AfterViewInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +7,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'weatherf';
-}
+  dcity:string=''
+  city:string=''
+  temp:string=''
+  text:string=''
+  feels:string=''
+  image:string=''
+  setCity(event:any)
+  {
+    this.city=event.target.value
+  }
+  async onSearch()
+  {
+    this.dcity=this.city;
+    try
+    {
+      const data=await fetch(`http://api.weatherapi.com/v1/current.json?key=58e489b4020d471494f181231250802&q=${this.dcity}`)
+      const res=await data.json();
+      this.image=res.current.condition.icon;
+      this.temp= res.current.temp_c + "°";
+      this.text=res.current.condition.text;
+      this.feels="feels like " + res.current.feelslike_c + "°";
+      console.log(res)
+  
+    }
+    catch(error)
+    {
+      console.error(error);
+    }
+  }
+
+  }
+  
+
